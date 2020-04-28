95c6852dd6430e66b354d2d00a680ebc9f247d11
github username <kateapault@gmail.com>
2020-04-28 16:54:44 -0400

    basic structure of home menu and simple encode option created

--- run.rb
+++ run.rb
@@ -1,7 +1,32 @@
-require_relative 'caesar'
+require_relative './ciphers/caesar'
+require 'tty-prompt'
+
+prompt = TTY::Prompt.new
+ciphers = %w(Caesar Placeholder)
 
 puts "welcome to CLIpto!"
-
-puts "example of Caesar encryption:"
-message, num = Caesar.encode('Here is an example message!')
-puts "with a shift of #{num}, your secret message is: #{message}"
+menu_choice = prompt.select("Choose an option:", %w(About Encode Decode Ciphers Exit))
+case menu_choice
+when "About"
+    puts "you chose 'About' - this page is coming soon!"
+when "Encode"
+    user_message = prompt.ask("Type your message: ")
+    cipher = prompt.select("Choose a cipher to encrypt your message: ", ciphers)
+    case cipher
+    when "Caesar"
+        message, num = Caesar.encode(user_message)
+        puts "with a shift of #{num}, your secret message is: #{message}"
+    when "Placeholder"
+        puts "this is really just a placeholder"
+    else
+        "Error: How did you even select this"
+    end
+when "Decode"
+    puts "not built yet"
+when "Ciphers"
+    puts "you chose 'Ciphers' - this page is coming soon!"
+when "Exit"
+    exit
+else 
+    puts "Error: invalid option chosen"
+end
