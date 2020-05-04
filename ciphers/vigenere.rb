40974513d3cbed49c03cc2ad7783eca6c2f44a0a
github username <kateapault@gmail.com>
2020-05-04 15:26:55 -0400

    added method to verify and format string in vigenere

--- ciphers/vigenere.rb
+++ ciphers/vigenere.rb
@@ -2,10 +2,21 @@
 class Vigenere
     @@alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+    def self.sanitize_string(message)
+        clean_message = ''
+
+        for i in 0...message.length
+            if @@alphabet.include? message[i].downcase()
+                clean_message += message[i].downcase()
+            end
+        end
+        return clean_message
+    end
+
     def self.encode_letter(letter,key_letter)
         letter_num = @@alphabet.index(letter)
         key_letter_num = @@alphabet.index(key_letter)
-        puts "letter: #{letter} | letter num: #{letter_num} | key: #{key_letter} | key num: #{key_letter_num}"
+        # puts "letter: #{letter} | letter num: #{letter_num} | key: #{key_letter} | key num: #{key_letter_num}"
 
         encoded_letter_num = letter_num + key_letter_num + 1
         encoded_letter_num > 26 ? encoded_letter_num -= 26 : encoded_letter_num
@@ -22,24 +33,26 @@ class Vigenere
     end
 
     def self.encode(message,key)
-        times_key_repeated = message.length / key.length + 1
-        key_message = (key * times_key_repeated)[0...message.length]
+        clean_message = self.sanitize_string(message)
+        times_key_repeated = clean_message.length / key.length + 1
+        key_message = (key * times_key_repeated)[0...clean_message.length]
 
         encoded_message = ''
-        for i in 0...message.length
-            encoded_message += self.encode_letter(message[i],key_message[i])
+        for i in 0...clean_message.length
+            encoded_message += self.encode_letter(clean_message[i],key_message[i])
         end
 
         return encoded_message
     end
 
     def self.decode(message,key)
-        times_key_repeated = message.length / key.length + 1
-        key_message = (key * times_key_repeated)[0...message.length]
+        clean_message = self.sanitize_string(message)
+        times_key_repeated = clean_message.length / key.length + 1
+        key_message = (key * times_key_repeated)[0...clean_message.length]
 
         decoded_message = ''
-        for i in 0...message.length
-            decoded_message += decode_letter(message[i],key_message[i])
+        for i in 0...clean_message.length
+            decoded_message += decode_letter(clean_message[i],key_message[i])
         end
 
         return decoded_message
