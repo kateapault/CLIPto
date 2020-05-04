627a82c6577851323a17188f62168244f7da5883
github username <kateapault@gmail.com>
2020-05-04 17:44:12 -0400

    corrected railfence encoding rail assignment

--- ciphers/railfence.rb
+++ ciphers/railfence.rb
@@ -4,17 +4,29 @@ class Railfence
         stripped_message = message.scan(/\w+/).join('').downcase
         rail_num = rand(2..stripped_message.length/2)
         encoded_message_arr = [''] * rail_num
+        rail_ind = 0
+        direction_up = true
         for i in 0...stripped_message.length
-            letter = stripped_message[i]
-            rail = i % rail_num
-            encoded_message_arr[rail] += letter
+            encoded_message_arr[rail_ind] += stripped_message[i]
+            # puts "#{encoded_message_arr}"
+            if rail_ind == rail_num -1                       # if at top rail
+                rail_ind -= 1                                   # start going down again
+                direction_up = false
+            elsif rail_ind == 0                              # if at bottom rail
+                rail_ind += 1                                   # start going up again
+                direction_up = true
+            elsif rail_ind < rail_num - 1 && direction_up    # if going up and ind below top rail
+                rail_ind += 1
+            else                                             # if going down and ind above bottom rail
+                rail_ind -= 1
+            end
         end
         return [encoded_message_arr.join(''), rail_num]
     end
 
     def self.decode(message,rail_num=nil)
         if rail_num
-            
+
         else 
 
         end
